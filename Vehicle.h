035118cbbe375b5a1ae98fcc9e10c81ca8725035
Norman Martin <normaneverthonmartin@gmail.com>
2024-11-03 20:49:29 -0500

    Modified Drvier Class

--- Vehicle.h
+++ Vehicle.h
@@ -1,6 +1,13 @@
-//Vehicle Class 
+// Vehicle Class
+#include <iostream>
+using namespace std;
 
-class Vehicle{
-    
-
+class Vehicle
+{
+private:
+    string curentDriver;
+    string previousDriver;
+    string currentOwner;
+    string PreviousOwner;
+    int licensePlateNumber;
 };
