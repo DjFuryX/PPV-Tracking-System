28bae364920d1a70531cdb1325529797b051d84e
Norman Martin <normaneverthonmartin@gmail.com>
2024-11-03 21:24:40 -0500

    modified:   Vehicle.h

--- Vehicle.h
+++ Vehicle.h
@@ -8,36 +8,47 @@ using namespace std;
 class Vehicle
 {
 private:
-    string curentDriver;
+    string currentDriver;
     string currentOwner;
     string previousDriver;
     string PreviousOwner;
-    int licensePlateNumber;
+    string licensePlateNumber;
 
 public:
     Vehicle() // default Constructor
     {
-        curentDriver = "notSet";
+        currentDriver = "notSet";
         currentOwner = "notSet";
         previousDriver = "notSet";
         PreviousOwner = "notSet";
-        licensePlateNumber = 0;
+        licensePlateNumber = "AA-0000";
     }
 
-    /// @brief Create a vehicle object
+    /// @brief Creates a vehicle object
     /// @param curr_driver current driver of vehicle
     /// @param prev_driver previous driver of vehicle
     /// @param curr_owner current owner of vehicle
     /// @param prev_owner previous owner of vehicle
     /// @param plateNumber License plate number of vehicle
-    Vehicle(int plateNumber,string curr_driver, string prev_driver, string curr_owner, string prev_owner)
+    Vehicle(string plateNumber, string curr_driver, string prev_driver, string curr_owner, string prev_owner)
     {
-
-        curentDriver =curr_driver;
+        licensePlateNumber = plateNumber;
+        currentDriver = curr_driver;
         currentOwner = curr_owner;
         previousDriver = prev_driver;
         PreviousOwner = prev_owner;
-        licensePlateNumber = 0;
+    }
+
+    string getCurrentDriver()
+    {
+
+        return currentDriver;
+    }
+
+    string getPreviousDriver()
+    {
+
+        return previousDriver;
     }
 };
 
