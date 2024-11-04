2bea1e93d861501e47048b83a1e398ab8bdc5c1b
hasani548 <124756408+hasani548@users.noreply.github.com>
2024-11-03 21:28:24 -0500

    ticket updateHM

--- Ticket.h
+++ Ticket.h
@@ -1,6 +1,51 @@
 //Ticket Class
 
-class Ticket{
-    
+#include<iostream>
+using namespace std;
+
+class Ticket
+{
+private:
+    int ticketNumber;
+    string ticketIssueDate;
+    string ticketOffenceCode;
+    string ticketOffenceDescription;
+    string licensePlateNumber;
+    float ticketAmount;
+    string dueDate;
+    string courtDate;
+    string issuingOfficer;
+    string courtLocation;
+
+    public:
+        Ticket()// default constructor
+       {
+        ticketNumber=0;
+        ticketIssueDate="notSet";
+        ticketOffenceCode="notSet";
+        ticketOffenceDescription="notSet";
+        licensePlateNumber="AA-0000";
+        ticketAmount=0.0;
+        dueDate="notSet";
+        courtDate="notSet";
+        issuingOfficer="notSet";
+        courtLocation="notSet";
+       }
+
+       Ticket(int ticNum, string issueDate, string offenceDes, string offenceCode, string plateNumber, 
+       float tAmount, string due, string court, string issueOfficer, string cLocation)
+       {
+        ticketNumber = ticNum;
+        ticketIssueDate = issueDate;
+        ticketOffenceCode = offenceCode;
+        ticketOffenceDescription = offenceDes;
+        licensePlateNumber = plateNumber;
+        ticketAmount = tAmount;
+        dueDate = due;
+        courtDate= court;
+        issuingOfficer= issueOfficer;
+        courtLocation= cLocation;
+       }
+
 
 };
