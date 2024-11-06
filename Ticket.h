1f3bceb28da98a11b9dd858acfca4d79cef28189
hasani548 <124756408+hasani548@users.noreply.github.com>
2024-11-05 20:55:41 -0500

    update mal

--- Ticket.h
+++ Ticket.h
@@ -52,6 +52,86 @@ private:
         courtLocation= cLocation;
        }
 
+       // Getters
+
+       int getTicketNumber(){
+            return ticketNumber;
+            }
+       string getTicketIssueDate(){ 
+            return ticketIssueDate; 
+            }
+       string getTicketOffenceCode(){
+            return ticketOffenceCode; 
+            }
+        string getTicketOffenceDescription(){ 
+            return ticketOffenceDescription; 
+            }
+        string getLicensePlateNumber(){ 
+            return licensePlateNumber; 
+            }
+        float getTicketAmount(){ 
+            return ticketAmount; 
+            }
+        string getDueDate(){ 
+            return dueDate; 
+            }
+        string getCourtDate(){ 
+            return courtDate; 
+            }
+        string getIssuingOfficer(){ 
+            return issuingOfficer; 
+            }
+        string getCourtLocation(){ 
+            return courtLocation; 
+            }
+
+        //Setter Methods
+
+        void setTicketNumber(int ticNum) { 
+            ticketNumber = ticNum; 
+            }
+        void setTicketIssueDate(string issueDate) { 
+            ticketIssueDate = issueDate; 
+            }
+        void setTicketOffenceCode(string offenceCode) { 
+            ticketOffenceCode = offenceCode; 
+            }
+        void setTicketOffenceDescription(string offenceDes) { 
+            ticketOffenceDescription = offenceDes; 
+            }
+        void setLicensePlateNumber(string plateNumber) { 
+            licensePlateNumber = plateNumber; 
+            }
+        void setTicketAmount(float tAmount) { 
+            ticketAmount = tAmount; 
+            }
+        void setDueDate(string due) { 
+            dueDate = due; 
+            }
+        void setCourtDate(string court) { 
+            courtDate = court; 
+            }
+        void setIssuingOfficer(string issueOfficer) { 
+            issuingOfficer = issueOfficer; 
+            }
+        void setCourtLocation(string cLocation) { 
+            courtLocation = cLocation; 
+            }
+
+
+        // Generates Ticket Report of Driver
+        void viewTicketReport(){
+            cout << "Ticket Bumber: "<< ticketNumber << endl;
+            cout << "Issue Date: " << ticketIssueDate << endl;
+            cout << "Offence Code: " <<ticketOffenceCode << endl;
+            cout << "Offence dEscription: " <<ticketOffenceDescription << endl;
+            cout << "License Plate Number: " << licensePlateNumber << endl;
+            cout << "Ticket Amount: $" << ticketAmount << endl;
+            cout << "Due Date: " << dueDate << endl;
+            cout << "Court Date: " << courtDate << endl;
+            cout << "Issuing Officer: " << issuingOfficer << endl;
+            cout << "Court Location: " << courtLocation << endl;
+        }
 
 };
 
