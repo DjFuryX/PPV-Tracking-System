101c338c737f20068dff5e9bd052107d37620c20
hasani548 <124756408+hasani548@users.noreply.github.com>
2024-11-05 20:59:48 -0500

    ticket done

--- Ticket.h
+++ Ticket.h
@@ -136,3 +136,22 @@ private:
 };
 
 #endif
+/* Main to Test
+#include <iostream>
+using namespace std;
+
+#include <Ticket.h>
+
+int main() {
+    // Using default constructor
+    Ticket defaultTicket;
+    cout << "Default Ticket Report:" << endl;
+    defaultTicket.viewTicketReport();
+
+    // Using parameterized constructor
+    Ticket ticket(12345, "2024-11-01", "Speeding", "SP123", "AB-1234", 150.0, "2024-11-30", "2024-12-15", "Officer Smith", "Downtown Court");
+    cout << "\nInitialized Ticket Report:" << endl;
+    ticket.viewTicketReport();
+
+    return 0;
+}*/
