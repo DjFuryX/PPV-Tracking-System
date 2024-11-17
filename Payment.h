cba87a0a4c8905dff4c069d0690ef0ad32d307ec
matmill034 <147534321+matmill034@users.noreply.github.com>
2024-11-17 18:20:03 -0500

    Update Payment.h

--- Payment.h
+++ Payment.h
@@ -1,6 +1,57 @@
 //Payment Class 
 
 class Payment{
-    
+
+    private:
+          int PaymentID;
+          string Payment_date;
+          float Payment_amount;
+          int Ticket_number;
+
+    public:
+       //default constructor//
+          Payment()
+          {
+             PaymentID = 0;
+             Payment_date = "";
+             Payment_amount = 0.00;
+             Ticket_number = 0;
+          }
+
+          //primary constructor
+          Payment (int payid, string paydate, float amount, int ticketnum)
+          {
+            PaymentID = payid;
+            Payment_date = paydate;
+            Payment_amount = amount;
+            Ticket_number = ticketnum;
+          }
+
+          //copy constructor
+          Payment (Payment &payment)
+          {
+            badgenumber = jcf.badgenumber;
+            firstname = jcf.firstname;
+            lastname = jcf.lastname;
+            assignedStation = jcf.assignedStation;
+          }
+           
+          //Function for calculating payment amount
+          void CalculatePayment()
+          {
+              
+          }
+
+          //Function to view each payment
+          void ViewPayment()
+          {
+
+          }
+          
 
 };
+
+int main()
+{
+
+}
