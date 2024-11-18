a9164df95d78a44320dce4e10d698fe7a2300777
Norman Martin <normaneverthonmartin@gmail.com>
2024-11-17 22:46:51 -0500

    Applicant class mostly Finished

--- Main.cpp
+++ Main.cpp
@@ -8,15 +8,12 @@ int main()
 {
   Applicant a2;
 
-  // a2.CreateBlankRecords();
+  a2.CreateBlankRecords();
   a2.Display();
-  a2.saveTest();
-  // a2.CreateApplication();
-  //  a2.SaveApplication();
+  a2.CreateApplication();
+  a2.SaveApplication();
 
-  //a2.retrieveApplication(100);
-
-  a2.Display();
+  a2.retrieveApplication(100);
 
   return 0;
 }
